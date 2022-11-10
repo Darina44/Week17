76ae1fbf9c7fc4eba285b91482e9fca5bc28f4b0
Darina44 <johndoe@email.com>
2022-11-10 23:11:33 +0300

    lessons

---
+++ index.js
@@ -0,0 +1,73 @@
+// let test = "Мы компания \"Google\"";
+// console.log(test);
+
+// let position = test.indexOf("Google");
+
+// console.log(position);
+
+// let sliced = test.slice(position, -1);
+
+// console.log(sliced);
+
+// let apple = test.replace(/google/i, "Apple");
+// console.log(apple);
+
+// let message = "Привет, username!";
+// let name = "Дарина";
+
+// let result = message.replace("username", name);
+
+// console.log(result);
+
+// let big = name.toUpperCase();
+// console.log(big);
+
+// let small = name.toLowerCase();
+// console.log(small);
+
+// let username = "Дарина";
+// if(name.toLowerCase() == username.toLowerCase()) {
+//     console.log(true);
+// }
+
+// console.log(name[0]);
+// console.log(name[name.length-1]);
+
+// let str = "   Москва";
+// let clean = str.trim(str);
+
+// console.log(clean);
+
+
+// let now = new Date();
+
+// console.log(now);
+
+// let timestamp = new Date(24*3600*1000);
+
+// let date = new Date("2022-10-10");
+// console.log(date);
+
+// let num = new Date(2022,10,10);
+// console.log(num.getMonth()+1);
+
+// num.setFullYear(2023);
+
+// console.log(num);
+
+// let diff = Date.now() - date;
+// console.log(diff / 1000 / 3600);
+
+console.log(Math.floor(0.01));
+console.log(Math.ceil(0.01));
+console.log(Math.round(0.01));
+console.log((0.91415667).toFixed(2));
+
+console.log(Math.round(Math.random()*100));
+
+let arr = [];
+arr.push(Math.round(Math.random()*100));
+arr.push(Math.round(Math.random()*100));
+arr.push(Math.round(Math.random()*100));
+arr.push(Math.round(Math.random()*100));
+console.log(arr);
